55f5751de4551c7564be96402c7e991c7080add2
jiangli <liminghaoze@qq.com>
2015-03-16 15:54:02 +0800

    修改爱奇艺mp4文件无法加载的bug，暂时解决方案

--- test.js
+++ test.js
@@ -11,6 +11,6 @@ var index = require('./index');
 
 
 //提供了url，直接运行node test.js
-index('http://www.iqiyi.com/v_19rrnbyreg.html','MP4',function(err,data){
+index('http://www.iqiyi.com/v_19rrnpsvrg.html#vfrm=2-3-0-1','MP4',function(err,data){
     console.log(data);
 });
